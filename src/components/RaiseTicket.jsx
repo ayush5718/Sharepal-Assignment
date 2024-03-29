f0d4c59f58a0e640bfd76c65129c4508b46e2123
ayush5718 <ayushbhardwaj9504@gmail.com>
2024-03-29 14:39:10 +0530

    first commit

--- src/components/RaiseTicket.jsx
+++ src/components/RaiseTicket.jsx
@@ -1,54 +1,46 @@
-import React from "react";
+import React, { useState } from "react";
 
-const RaiseTicket = () => {
+const RaiseTicket = ({ onClose }) => {
   return (
     <div>
       <>
         {/* Main modal */}
-        <div
-          id="crud-modal"
-          tabIndex={1}
-          aria-hidden="true"
-          className=" overflow-y-auto overflow-x-hidden fixed top-0 right-0 left-0 z-50 justify-center items-center w-full md:inset-0 h-[calc(100%-1rem)] max-h-full"
-        >
-          <div className="relative p-4 w-full max-w-md max-h-full">
-            {/* Modal content */}
-            <div className="relative bg-white rounded-lg shadow dark:bg-gray-700">
-              {/* Modal header */}
-              <div className="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 className="text-lg font-semibold text-gray-900 dark:text-white">
-                  Create New Product
-                </h3>
+        <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-filter backdrop-blur-lg">
+          <div className="flex items-center justify-center min-h-full p-4 text-center">
+            <div className="relative w-full max-w-lg p-4 md:p-8 bg-white rounded-3xl shadow-lg">
+              {/* Close button and modal header */}
+              <div className="flex items-start justify-between">
+                <h3 className="text-lg font-semibold">Create New Product</h3>
                 <button
                   type="button"
-                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm w-8 h-8 ms-auto inline-flex justify-center items-center dark:hover:bg-gray-600 dark:hover:text-white"
+                  className="text-gray-400 bg-transparent hover:bg-gray-200 hover:text-gray-900 rounded-lg text-sm p-1.5 ml-auto inline-flex items-center"
                   data-modal-toggle="crud-modal"
+                  onClick={onClose}
                 >
+                  <span className="sr-only">Close</span>
                   <svg
-                    className="w-3 h-3"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
+                    className="w-6 h-6"
                     fill="none"
-                    viewBox="0 0 14 14"
+                    stroke="currentColor"
+                    viewBox="0 0 24 24"
                   >
                     <path
-                      stroke="currentColor"
                       strokeLinecap="round"
                       strokeLinejoin="round"
-                      strokeWidth={2}
-                      d="m1 1 6 6m0 0 6 6M7 7l6-6M7 7l-6 6"
-                    />
+                      strokeWidth="2"
+                      d="M6 18L18 6M6 6l12 12"
+                    ></path>
                   </svg>
-                  <span className="sr-only">Close modal</span>
                 </button>
               </div>
+
               {/* Modal body */}
-              <form className="p-4 md:p-5">
+              <form className="mt-5">
                 <div className="grid gap-4 mb-4 grid-cols-2">
                   <div className="col-span-2">
                     <label
                       htmlFor="name"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900 text-left"
                     >
                       Name
                     </label>
@@ -56,7 +48,7 @@ const RaiseTicket = () => {
                       type="text"
                       name="name"
                       id="name"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      className="bg-white border-1 border-gray-700 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 text-gray-800 dark:focus:ring-primary-500 dark:focus:border-primary-500"
                       placeholder="Type product name"
                       required=""
                     />
@@ -64,7 +56,7 @@ const RaiseTicket = () => {
                   <div className="col-span-2 sm:col-span-1">
                     <label
                       htmlFor="price"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900 text-left"
                     >
                       Price
                     </label>
@@ -72,7 +64,7 @@ const RaiseTicket = () => {
                       type="number"
                       name="price"
                       id="price"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400  dark:focus:ring-primary-500 dark:focus:border-primary-500"
                       placeholder="$2999"
                       required=""
                     />
@@ -80,13 +72,13 @@ const RaiseTicket = () => {
                   <div className="col-span-2 sm:col-span-1">
                     <label
                       htmlFor="category"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900 text-left"
                     >
                       Category
                     </label>
                     <select
                       id="category"
-                      className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                      className="bg-white border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 text-gray-400 dark:focus:ring-primary-500 dark:focus:border-primary-500"
                     >
                       <option selected="">Select category</option>
                       <option value="TV">TV/Monitors</option>
@@ -98,14 +90,14 @@ const RaiseTicket = () => {
                   <div className="col-span-2">
                     <label
                       htmlFor="description"
-                      className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                      className="block mb-2 text-sm font-medium text-gray-900  text-left"
                     >
                       Product Description
                     </label>
                     <textarea
                       id="description"
                       rows={4}
-                      className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                      className="block p-2.5 w-full text-sm text-gray-900 bg-white rounded-lg border border-gray-300 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400  dark:focus:ring-blue-500 dark:focus:border-blue-500"
                       placeholder="Write product description here"
                       defaultValue={""}
                     />
@@ -127,7 +119,7 @@ const RaiseTicket = () => {
                       clipRule="evenodd"
                     />
                   </svg>
-                  Add new product
+                  Raise Ticket
                 </button>
               </form>
             </div>
--- src/pages/MyOrdersPage.jsx
+++ src/pages/MyOrdersPage.jsx
@@ -1,5 +1,5 @@
 // src/pages/MyOrdersPage.js
-import React from "react";
+import React, { useState } from "react";
 import { MyOrders } from "../components/MyOrders";
 import OrderStage from "../components/OrdersStage";
 import RaiseTicket from "../components/RaiseTicket";
@@ -7,6 +7,13 @@ import CardPopup from "../components/CardPopup";
 
 // import CardPopup from "../components/CardPopup";
 const MyOrdersPage = () => {
+  const [showRaiseTicket, setshowRaiseTicket] = useState(null);
+  const handleOpenTicketModal = () => {
+    setshowRaiseTicket(1);
+  };
+  const handleCloseTicketModal = () => {
+    setshowRaiseTicket(null);
+  };
   return (
     <div>
       <h1 className="text-center">My Orders</h1>
@@ -14,6 +21,13 @@ const MyOrdersPage = () => {
       {/* <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-filter backdrop-blur-lg">
         <RaiseTicket />
       </div> */}
+
+      <button onClick={handleOpenTicketModal}>open raise ticket</button>
+      {showRaiseTicket && (
+        <div className="fixed inset-0 flex items-center justify-center z-50 backdrop-filter backdrop-blur-lg">
+          <RaiseTicket onClose={handleCloseTicketModal} />
+        </div>
+      )}
 
       {/* <OrderStage /> */}
       {/* <CardPopup /> */}
